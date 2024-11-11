740dc8c43fde12a03a58ebb3af12149390cdab23
Diwash Nembang <diwashnembang2@gmail.com>
2024-11-11 23:18:18 +1100

    added guide page in popup

--- src/popup.ts
+++ src/popup.ts
@@ -4,16 +4,24 @@ const PopUpPort = chrome.runtime.connect({ name: "popup" });
 
 (async () => {
   PopUpPort.postMessage({ action: "getTabs" });
-  
+
   PopUpPort.onMessage.addListener((response) => {
     try {
       let tabs: Record<number | string, chrome.tabs.Tab> = response;
-      
+
       if (!tabList) {
         console.log("cannot render tabs in popup: tabList is null");
         return;
       }
-      
+
+      if (Object.keys(tabs).length === 0) {
+        {
+          // Redirect to a different page within the same site
+          window.location.href = "learn.html";
+          return;
+        }
+     }
+
       tabList.innerHTML = ""; // Clear existing items
 
       // Iterate over the object using Object.entries()
@@ -22,24 +30,26 @@ const PopUpPort = chrome.runtime.connect({ name: "popup" });
         const listItem = document.createElement("li");
 
         const icon = document.createElement("img");
-        icon.src = "https://www.techonthenet.com/clipart/keyboard/images/shift_key.png"; // Update with your image path
+        icon.src =
+          "https://www.techonthenet.com/clipart/keyboard/images/shift_key.png"; // Update with your image path
         icon.alt = "Tab Icon"; // Alt text for accessibility
         icon.style.width = "20px"; // Set desired width
         icon.style.height = "20px"; // Set desired height
         icon.style.marginRight = "8px"; // Space between icon and text
 
-        const keyNode = document.createElement("span");
-    
-        keyNode.textContent = `${key} +`
+        const keyNode = document.createElement("code");
+
+        keyNode.textContent = `${key} + Shift`;
         // Create a text node for the tab information
         const textNode = document.createTextNode(`${tab.url}`);
+        const url = document.createElement("span");
+        url.classList.add("url_span");
+        url.appendChild(textNode);
 
         // Append the icon and text to the list item
         listItem.appendChild(keyNode);
-        listItem.appendChild(icon);
-        listItem.appendChild(textNode);
-
-        
+        // listItem.appendChild(icon);
+        listItem.appendChild(url);
 
         // Create the delete button
         const deleteButton = document.createElement("button");
@@ -62,7 +72,6 @@ const PopUpPort = chrome.runtime.connect({ name: "popup" });
       });
     } catch (e) {
       console.log("Couldn't get tabs", e);
-    } 
+    }
   });
 })();
-
